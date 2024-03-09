7180a0717d3bb13b8b79a5850cbd76fa283b3390
Mateusz <mateusz.bryla@edu.uekat.pl>
2024-03-09 13:33:09 +0100

    adding tasks

---
+++ 02_03_2024/zad3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Funkcja do obliczania silni
+long long silnia(int n) {
+    if (n <= 1)
+        return 1;
+    else
+        return n * silnia(n - 1);
+}
+
+// Funkcja do obliczania silni dla ka?dego elementu tablicy
+void obliczSilnie(int array[], int rozmiar) {
+    for (int i = 0; i < rozmiar; i++) {
+        printf("Silnia liczby %d wynosi: %lld\n", array[i], silnia(array[i]));
+    }
+}
+
+int main() {
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int rozmiar = sizeof(array) / sizeof(array[0]); // Obliczamy rozmiar tablicy
+
+    obliczSilnie(array, rozmiar);
+
+    return 0;
+}
---
+++ 02_03_2024/zad5.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+// Funkcja obliczająca sumę wielokrotności 3 lub 5
+int sumaWielokrotnosci(int limit) {
+    int suma = 0;
+    // Zwróć 0, jeśli limit jest ujemny
+    if (limit < 0) {
+        return 0;
+    }
+    // Przejdź przez wszystkie liczby poniżej limitu
+    for (int i = 1; i < limit; i++) {
+        // Sprawdź, czy liczba jest wielokrotnością 3 lub 5
+        if (i % 3 == 0 || i % 5 == 0) {
+            suma += i;
+        }
+    }
+    return suma;
+}
+int main() {
+    int limit;
+    printf("Podaj limit: ");
+    scanf("%d", &limit);
+
+    int wynik = sumaWielokrotnosci(limit);
+    printf("Suma wielokrotności 3 lub 5 poniżej %d wynosi: %d\n", limit, wynik);
+
+    return 0;
+}
