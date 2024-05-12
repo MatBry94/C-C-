c5cd8f654004b791f07b11cf6a4d8f151c4d7df1
Mateusz <mateusz.bryla@edu.uekat.pl>
2024-05-12 12:47:54 +0200

    add bubblesort

--- Zadania4/zad2.c
+++ Zadania4/zad2.c
@@ -1,4 +1,33 @@
-﻿int main() {
+﻿#include <stdio.h>
+
+// Funkcja sortująca tablicę metodą sortowania bąbelkowego
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+// Funkcja wyszukiwania binarnego w posortowanej tablicy
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
     int arr[11];
     int key;
 
